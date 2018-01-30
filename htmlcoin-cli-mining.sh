4f7f0d604ef13cf26d0ff314ec0b8d2ab2a4570c
hoonkoh <hoonkoh0518@gmail.com>
2018-01-30 18:40:04 +0900

    start hoonkoh project!

---
+++ htmlcoin-cli-mining.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# if this file is not executable, plese make sure this file is executable
+# copy the line below without the # sign and press enter
+# chmod +x htmlcoin-wallet-mining.sh
+# now you can run this file by typing
+# ./htmlcoin-wallet-miining.sh
+
+clear
+count=1
+
+echo "HTMLCoin wallet mining started..."
+echo
+
+# change directory into wallet folder. If the folder name is different, then change to the correct folder name
+cd HTMLCOIN
+
+while true; do
+  echo "$count: $(date)"
+  echo "$count: $(date)" >> wallet-mining.log
+
+  # change the address to your address
+  src/htmlcoin-cli generatetoaddress 100 HrRUR6q4AZDMTrz3kAPxSEiGaJkycSndDs 999999999 >> wallet-mining.log
+  (( count++ ))
+done
